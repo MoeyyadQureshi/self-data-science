d5f602756b3302c59a863adfed8954e313461b5b
Moeyyad Qureshi <moeyyadqureshi@gmail.com>
2018-05-09 19:25:40 -0400

    save and loadfeature sketchup

--- data.js
+++ data.js
@@ -3,18 +3,22 @@ var fs = require("fs")
 var dialog = app.dialog
 
 document.getElementById("data-submit").onclick = () => {
-    dialog.showSaveDialog((filename) => {
-        if (filename === undefined){
-            alert("You didn't save the file")
+    var content = document.getElementById("data-entry").value
+    fs.writeFile("dummy.txt", content, (err) => {
+        if (err) console.log(err)
+        alert("The file has been succesfully saved")
+    })
+}
+
+document.getElementById("data-retrieve").onclick = () => {
+    fs.readFile("dummy.txt", "utf-8", (err, data) => {
+        if (err) {
+            console.log(err)
             return
         }
 
-        // TODO: create/update save file without asking user to choose save location
-        var content = document.getElementById("data-entry").value
-        fs.writeFile(filename, content, (err) => {
-            if (err) console.log(err)
-            alert("The file has been succesfully saved")
-        })
+        var textbox = document.getElementById("data-entry")
+        textbox.value = data
     })
 }
 
